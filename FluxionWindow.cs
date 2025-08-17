b491b1660d534fd7ff520ea37754f20926ecd0ba
Mirrihk <sebas6721@gmail.com>
2025-08-16 23:19:37 -0400

    Add project files.

---
+++ FluxionWindow.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+public sealed class FluxionWindow : GameWindow
+{
+    public FluxionWindow(GameWindowSettings gws, NativeWindowSettings nws)
+        : base(gws, nws)
+    {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        GL.ClearColor(Color4.Black); // background color
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+        // Here you’ll update physics / math each frame
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        // Example: draw something simple (we’ll add more later)
+
+        SwapBuffers(); // show frame
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Windowing.Desktop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var native = new NativeWindowSettings()
+        {
+            Title = "Fluxion Engine",
+            Size = new OpenTK.Mathematics.Vector2i(1280, 720)
+        };
+
+        using var window = new FluxionWindow(GameWindowSettings.Default, native);
+        window.Run();
+    }
+}
