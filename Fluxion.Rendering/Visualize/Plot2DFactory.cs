2e542fbf8be4012f8f9fef0c8205f1b45d88be87
Mirrihk <sebas6721@gmail.com>
2025-08-17 23:36:57 -0400

    upadte for methods that are called to visualize math

---
+++ Fluxion.Rendering/Visualize/Plot2DFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxion.Fluxion.Rendering.Visualize
+{
+    internal class Plot2DFactory
+    {
+    }
+}
