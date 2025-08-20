4223fff530aac69b5d59b4eaa79a12d85e5aa533
Mirrihk <sebas6721@gmail.com>
2025-08-19 21:57:32 -0400

    addedd a consle render feautures to control line size etc

--- Fluxion.Core/App/Program.cs
+++ Fluxion.Core/App/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Fluxion.Core/App/Program.cs
 using Fluxion.Features.Algebra;
+using Fluxion.Math.Algebra.Solvers;
+using Fluxion.Rendering.Draw;
+using Fluxion.Rendering.Visualize;
 
 namespace Fluxion.Core.App
 {
@@ -7,16 +10,19 @@ namespace Fluxion.Core.App
     {
         public static void Main(string[] args)
         {
-            // Same as before
-            //LinearFeature.SolveAndPrint(A: 5, B: -15);
-           // LinearFeature.Graph(A: 5, B: -15, xMin: -2, xMax: 6);
+            ///Same as before
+           // LinearFeature.SolveAndPrint(A: 5, B: -15);
+            //LinearFeature.Graph(A: 5, B: -15, xMin: -2, xMax: 6);
 
             // NEW: show steps (single-side)
-            // LinearFeature.ShowSteps(A: 5, B: -15);
+            //LinearFeature.ShowSteps(A: 5, B: -15);
 
             // NEW: show steps (two-sided): 12.6 + 4x = 9.6 + 8x
-            LinearFeature.ShowSteps(a: 4, b: 12.6, c: 8, d: 9.6);
+            LinearFeature.ShowSteps(a: 7, b: 0.4, c: 6, d: 1.4);
+
+
+            // Create an instance of a renderer
+
         }
     }
 }
- 
